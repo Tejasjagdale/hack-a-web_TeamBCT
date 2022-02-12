c022a901d68c56d19b2838377fe920915dae962f
BryanRonad <bryanronad@gmail.com>
2022-02-12 17:18:37 +0530

    Added dynamic event pushing and button disable

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,5 +1,5 @@
 import { Button, Input } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { doc, setDoc } from "firebase/firestore";
 import { useAuth } from "../context/AuthContext";
 import { db } from "../utils/firebase-config";
@@ -14,34 +14,54 @@ import {
 const Events = () => {
   const { currentUser } = useAuth();
   const ref = db.collection("events");
-
-  const AddEvents = async (e) => {
-    e.preventDefault();
-    var d = new Date();
-
-    let data = {
+  const [event, setEvent] = useState({
       created_by: currentUser.uid,
-      created_on: d.toLocaleString(),
-      items: ["12x424124c1"],
+      created_on: "",
+      items: [],
       current_users: [],
       total_users: [],
-      name: "auction 1",
-    };
+      name: "",
+    })
+  let name, value
 
-    await ref.add(data);
+  useEffect(() => {
+    console.log(event);
+  }, [event])
+  
+
+  const handleEventChange = (e) => {
+    name = e.target.name
+    value = e.target.value
+    // console.log(name, value);
+    setEvent((prevState) => {
+      return {...prevState, [name]: value}
+    })
+  }
+
+  const addEvent = async (e) => {
+    e.preventDefault();
+    var d = new Date();
+    let event_push = event
+    event_push = { ...event_push, created_by: currentUser.uid, created_on: d.toLocaleString()}
+    await ref.add(event_push);
   };
+
+  const addItem = async (e) => {
+    e.preventDefault()
+    var d = new Date()
+  }
 
   return (
     <>
       <Navbar />
       <FormControl>
         <FormLabel >Event name</FormLabel>
-        <Input id="name" type="text" />
+        <Input id="name" name="name" type="text" onChange={handleEventChange}/>
       </FormControl>
-      <Button colorScheme="teal" size="md" onClick={AddEvents}>
+      <Button isDisabled={event.items.length === 0} colorScheme="teal" size="md" onClick={addEvent}>
         Start New Event
       </Button>
-      <Button colorScheme="red" size="md" onClick={AddEvents}>
+      <Button colorScheme="red" size="md" onClick={addItem}>
         Add items
       </Button>
     </>
