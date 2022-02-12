fb0d71c709ca42919495f950c60efc767e20ad12
Tejasjagdale <69722269+Tejasjagdale@users.noreply.github.com>
2022-02-13 02:02:24 +0530

    allevents page

--- src/pages/AuctionRoom.jsx
+++ src/pages/AuctionRoom.jsx
@@ -8,35 +8,35 @@ import UserCard from "../components/UserCard";
 import UserControls from "../components/UserControls";
 
 const AuctionRoom = () => {
-	return (
-		<>
-			<Grid
-				h="100vh"
-				templateRows="repeat(12, 1fr)"
-				templateColumns="repeat(12, 1fr)"
-				gap={1}
-			>
-				<GridItem rowSpan={4} colSpan={12} mt={2}>
-					<Card p={2} overflow="hidden">
-						<Flex direction="row" justifyContent="space-between">
-							<ItemViewer />
-							<ItemDetails />
-						</Flex>
-					</Card>
-				</GridItem>
-				<GridItem rowSpan={5} colSpan={12}>
-					<Card overflow="hidden">
-						<Audience />
-					</Card>
-				</GridItem>
-				<GridItem rowSpan={3} colSpan={12}>
-					<Flex direction="row" justifyContent="space-apart">
-						<UserControls />
-					</Flex>
-				</GridItem>
-			</Grid>
-		</>
-	);
+  return (
+    <>
+      <Grid
+        h="100vh"
+        templateRows="repeat(12, 1fr)"
+        templateColumns="repeat(12, 1fr)"
+        gap={1}
+      >
+        <GridItem rowSpan={4} colSpan={12} mt={2}>
+          <Card p={2} overflow="hidden">
+            <Flex direction="row" justifyContent="space-between">
+              <ItemViewer />
+              <ItemDetails />
+            </Flex>
+          </Card>
+        </GridItem>
+        <GridItem rowSpan={5} colSpan={12}>
+          <Card overflow="hidden">
+            <Audience />
+          </Card>
+        </GridItem>
+        <GridItem rowSpan={3} colSpan={12}>
+          <Flex direction="row" justifyContent="space-apart">
+            <UserControls />
+          </Flex>
+        </GridItem>
+      </Grid>
+    </>
+  );
 };
 
 export default AuctionRoom;
