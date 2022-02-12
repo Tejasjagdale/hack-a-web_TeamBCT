0bae939e56e51bff86431d79f36d4583ffc05141
Tejasjagdale <69722269+Tejasjagdale@users.noreply.github.com>
2022-02-12 18:36:29 +0530

    add items fet

---
+++ src/components/ItemsCard.jsx
@@ -0,0 +1,175 @@
+import {
+  AspectRatio,
+  Box,
+  Button,
+  Container,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  Image,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+
+const ItemsCard = (props) => {
+  return (
+    <>
+      <Container maxW="80rem" centerContent>
+        <SimpleGrid columns={[1, 1, 1, 1]}>
+          <Box
+            p={4}
+            display={{ md: "flex" }}
+            maxWidth="32rem"
+            borderWidth={1}
+            margin={2}
+          >
+            <Image
+              maxWidth="200px"
+              margin="auto"
+              src="https://vinc.gumlet.io/gallery/blog/thumbs/bicycle-playing-card-trick-decks.jpg"
+              alt="Woman paying for a purchase"
+            />
+            <Stack
+              align={{ base: "center", md: "stretch" }}
+              textAlign={{ base: "center", md: "left" }}
+              mt={{ base: 4, md: 0 }}
+              ml={{ md: 6 }}
+            >
+              <Editable
+                fontWeight="bold"
+                fontSize="lg"
+                letterSpacing="wide"
+                color="teal.600"
+                value={props.item.name}
+              >
+                <EditablePreview />
+                <EditableInput name="name" onChange={props.handleItemchange} />
+              </Editable>
+
+              <Editable
+                my={1}
+                display="block"
+                fontSize="md"
+                lineHeight="normal"
+                fontWeight="semibold"
+                value={props.item.description}
+              >
+                <EditablePreview />
+                <EditableInput
+                  name="description"
+                  onChange={props.handleItemchange}
+                />
+              </Editable>
+
+              <Editable my={2} color="gray.500" value={props.item.base_Prize}>
+                <EditablePreview />
+                <EditableInput
+                  name="base_Prize"
+                  onChange={props.handleItemchange}
+                />
+              </Editable>
+
+              <Editable my={2} color="gray.500" value={props.item.bid_timer}>
+                <EditablePreview />
+                <EditableInput
+                  name="bid_timer"
+                  onChange={props.handleItemchange}
+                />
+              </Editable>
+
+              <Button colorScheme="red" size="md" onClick={props.addItem}>
+                Add Item
+              </Button>
+            </Stack>
+          </Box>
+          {props.list.map(function (data) {
+            return (
+              <Box
+                p={4}
+                display={{ md: "flex" }}
+                maxWidth="32rem"
+                borderWidth={1}
+                margin={2}
+                key={data.id}
+              >
+                <Image
+                  maxWidth="200px"
+                  margin="auto"
+                  src="https://vinc.gumlet.io/gallery/blog/thumbs/bicycle-playing-card-trick-decks.jpg"
+                  alt="Woman paying for a purchase"
+                />
+                <Stack
+                  align={{ base: "center", md: "stretch" }}
+                  textAlign={{ base: "center", md: "left" }}
+                  mt={{ base: 4, md: 0 }}
+                  ml={{ md: 6 }}
+                >
+                  <Editable
+                    fontWeight="bold"
+                    fontSize="lg"
+                    letterSpacing="wide"
+                    color="teal.600"
+                    value={data.name}
+                  >
+                    <EditablePreview />
+                    <EditableInput
+                      name="name"
+                      onChange={props.handleItemchange}
+                    />
+                  </Editable>
+
+                  <Editable
+                    my={1}
+                    display="block"
+                    fontSize="md"
+                    lineHeight="normal"
+                    fontWeight="semibold"
+                    value={data.description}
+                  >
+                    <EditablePreview />
+                    <EditableInput
+                      name="description"
+                      onChange={props.handleItemchange}
+                    />
+                  </Editable>
+
+                  <Editable
+                    my={2}
+                    color="gray.500"
+                    value={data.base_Prize}
+                  >
+                    <EditablePreview />
+                    <EditableInput
+                      name="base_Prize"
+                      onChange={props.handleItemchange}
+                    />
+                  </Editable>
+
+                  <Editable
+                    my={2}
+                    color="gray.500"
+                    value={data.bid_timer}
+                  >
+                    <EditablePreview />
+                    <EditableInput
+                      name="bid_timer"
+                      onChange={props.handleItemchange}
+                    />
+                  </Editable>
+                  <Button maxWidth="100px" my={2}>
+                    Click me!
+                  </Button>
+                </Stack>
+              </Box>
+            );
+          })}
+        </SimpleGrid>
+      </Container>
+    </>
+  );
+};
+
+export default ItemsCard;
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -10,60 +10,93 @@ import {
   FormErrorMessage,
   FormHelperText,
 } from "@chakra-ui/react";
+import ItemsCard from "../components/ItemsCard";
 
 const Events = () => {
   const { currentUser } = useAuth();
   const ref = db.collection("events");
   const [event, setEvent] = useState({
-      created_by: currentUser.uid,
-      created_on: "",
-      items: [],
-      current_users: [],
-      total_users: [],
-      name: "",
-    })
-  let name, value
+    created_by: currentUser.uid,
+    created_on: "",
+    items: [],
+    current_users: [],
+    total_users: [],
+    name: "",
+  });
+
+  const [item, setItem] = useState({
+    photos: [],
+    base_Prize: "0 rs",
+    description: "Item Description in 10-20 words :)",
+    bid_timer: "0 sec",
+    status: "hold",
+    name: "Item Name",
+  });
+
+  let name, value;
 
   useEffect(() => {
-    console.log(event);
-  }, [event])
-  
+    console.log(event,item);
+  }, [event,item]);
 
   const handleEventChange = (e) => {
-    name = e.target.name
-    value = e.target.value
+    name = e.target.name;
+    value = e.target.value;
     // console.log(name, value);
     setEvent((prevState) => {
-      return {...prevState, [name]: value}
-    })
-  }
+      return { ...prevState, [name]: value };
+    });
+  };
 
   const addEvent = async (e) => {
     e.preventDefault();
     var d = new Date();
-    let event_push = event
-    event_push = { ...event_push, created_by: currentUser.uid, created_on: d.toLocaleString()}
+    let event_push = event;
+    event_push = {
+      ...event_push,
+      created_by: currentUser.uid,
+      created_on: d.toLocaleString(),
+    };
     await ref.add(event_push);
   };
 
   const addItem = async (e) => {
     e.preventDefault()
-    var d = new Date()
-  }
+    item.id = event.items.length + 1;
+    setEvent((prevState) => {
+      return { ...prevState, ["items"]: [...prevState.items,item] };
+    });
+  };
 
+  const handleItemchange = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    // console.log(name, value);
+    setItem((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
   return (
     <>
       <Navbar />
       <FormControl>
-        <FormLabel >Event name</FormLabel>
-        <Input id="name" name="name" type="text" onChange={handleEventChange}/>
+        <FormLabel>Event name</FormLabel>
+        <Input id="name" name="name" type="text" onChange={handleEventChange} />
       </FormControl>
-      <Button isDisabled={event.items.length === 0} colorScheme="teal" size="md" onClick={addEvent}>
+      <Button
+        isDisabled={event.items.length === 0}
+        colorScheme="teal"
+        size="md"
+        onClick={addEvent}
+      >
         Start New Event
       </Button>
-      <Button colorScheme="red" size="md" onClick={addItem}>
-        Add items
-      </Button>
+      <ItemsCard
+        list={event.items}
+        addItem={addItem}
+        item={item}
+        handleItemchange={handleItemchange}
+      />
     </>
   );
 };
