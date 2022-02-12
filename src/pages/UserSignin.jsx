3b32c628cef21f7b5f919d610aa6ee34a58621ee
Cris10Lobo <cris10lobo@gmail.com>
2022-02-12 23:12:31 +0530

    Added spinner during load

--- src/pages/UserSignin.jsx
+++ src/pages/UserSignin.jsx
@@ -1,9 +1,8 @@
-import React, { useContext } from "react";
+import React from "react";
 import { db } from "../utils/firebase-config";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import SignInCard from "../components/SignInCard";
-import Header from "../components/Header";
 
 const UserSignin = () => {
 	const { signInWithGoogle } = useAuth();
